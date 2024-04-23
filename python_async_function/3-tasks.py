5d64e9282e81592174474e49302571bdd8bfe342
GillesR76 <gilles-richard76@hotmail.com>
2024-04-23 13:43:28 +0200

    Add type hint for task_wait_random function in python_async_function

--- python_async_function/3-tasks.py
+++ python_async_function/3-tasks.py
@@ -9,5 +9,8 @@ import asyncio
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-def task_wait_random(max_delay):
+def task_wait_random(max_delay: int) -> asyncio.Task:
+    """function that takes an integer max_delay
+    and returns a asyncio.Task
+    """
     return asyncio.Task(wait_random(max_delay))
