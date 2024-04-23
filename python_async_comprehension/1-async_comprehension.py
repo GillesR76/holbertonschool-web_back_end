37d6bf9ba722175aae7c610177136bda9a9931ba
GillesR76 <gilles-richard76@hotmail.com>
2024-04-23 14:11:10 +0200

    Refactor async_comprehension function to use async comprehension syntax and append numbers from async_generator in python_async_comprehension

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -1,18 +1,18 @@
 #!/usr/bin/env python3
-
-
-"""Async Comprehensions"""
-
+"""write a coroutine called async_comprehension that takes no arguments.
+The coroutine will collect 10 random numbers using an async comprehensing
+over async_generator, then return the 10 random numbers.
+"""
 
 from typing import List
-
-
 async_generator = __import__('0-async_generator').async_generator
 
 
 async def async_comprehension() -> List[float]:
+    """coroutine that collects 10 random numbers using an
+    async comprehensing then return the 10 random numbers
     """
-    Collecte 10 nombres aléatoires en utilisant une async
-    comprehension sur async_generator.
-    """
-    return [i async for i in async_generator()]
+    list_nbrs = []
+    async for number in async_generator():
+        list_nbrs.append(number)
+    return list_nbrs
