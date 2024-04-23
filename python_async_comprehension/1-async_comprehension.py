d501440c20e0f5aec39be5576645b97e40d4c6b7
GillesR76 <gilles-richard76@hotmail.com>
2024-04-23 13:57:57 +0200

    Refactor async_comprehension function to include type hint for return value in python_async_comprehension

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -4,11 +4,11 @@ The coroutine will collect 10 random numbers using an async comprehensing
 over async_generator, then return the 10 random numbers.
 """
 
-import asyncio
+from typing import List
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension():
+async def async_comprehension() -> List[float]:
     """coroutine that collects 10 random numbers using an
     async comprehensing then return the 10 random numbers
     """
