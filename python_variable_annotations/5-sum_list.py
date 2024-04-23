4a4f5d6be52ea96d3adcf03e5590b0fd6d1ad53a
GillesR76 <gilles-richard76@hotmail.com>
2024-04-23 13:25:09 +0200

    Add type hint for sum_list function in python_variable_annotations

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -7,4 +7,5 @@ input_list of floats as argument and returns their sum as a float.
 
 
 def sum_list(input_list: list[float]) -> float:
+    """returns their sum as a float"""
     return sum(input_list)
