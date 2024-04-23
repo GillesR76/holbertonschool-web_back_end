b1a2a5e2d0b4cba4ce88dc312f3e51de66b2c187
GillesR76 <gilles-richard76@hotmail.com>
2024-04-23 13:45:37 +0200

    Refactor measure_time function to use asyncio.run instead of await in python_async_function

--- python_async_function/2-measure_runtime.py
+++ python_async_function/2-measure_runtime.py
@@ -17,7 +17,7 @@ async def measure_time(n: int, max_delay: int) -> float:
     and returns total_time / n
     """
     start_time = time.time()
-    await wait_n(n, max_delay)
+    asyncio.run(wait_n(n, max_delay))
     end_time = time.time()
     total_time = end_time - start_time
     return total_time / n
