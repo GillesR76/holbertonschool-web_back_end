1ef807aaf9a77ad179b6a9ee3c6d38e7197713bf
GillesR76 <gilles-richard76@hotmail.com>
2024-04-23 14:06:22 +0200

    Refactor async_comprehension function to append numbers from async_generator in python_async_comprehension

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -12,4 +12,7 @@ async def async_comprehension() -> List[float]:
     """coroutine that collects 10 random numbers using an
     async comprehensing then return the 10 random numbers
     """
-    return [i async for i in async_generator()]
+    list_nbrs = [i async for i in async_generator()]
+    async for number in async_generator():
+        list_nbrs.append(number)
+    return list_nbrs
