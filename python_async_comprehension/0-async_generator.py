e2a8a67283de305426a580af003bfd40f855794b
GillesR76 <gilles-richard76@hotmail.com>
2024-04-23 13:54:33 +0200

    Refactor async_generator function to include type hint for return value in python_async_comprehension

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -6,9 +6,13 @@ second, then yield a random number between 0 and 10. Use the random module.
 
 import asyncio
 import random
+from typing import Generator
 
 
-async def async_generator():
+async def async_generator() -> Generator[float, None, None]:
+    """coroutine that loops 10 times, each time asynchronously
+    then yields a random number second
+    """
     for number in range(10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
