c674255e42a237d46b35c706278e1dd9ee067f16
GillesR76 <gilles-richard76@hotmail.com>
2024-04-23 14:04:32 +0200

    Refactor async_comprehension function to use async comprehension in python_async_comprehension

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -12,7 +12,5 @@ async def async_comprehension() -> List[float]:
     """coroutine that collects 10 random numbers using an
     async comprehensing then return the 10 random numbers
     """
-    list_nbrs = []
-    async for number in async_generator():
-        list_nbrs.append(number)
+    list_nbrs = [number async for number in async_generator()]
     return list_nbrs
