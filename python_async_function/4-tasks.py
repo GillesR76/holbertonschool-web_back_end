28a276a418c0212e296773706120d872206593fa
GillesR76 <gilles-richard76@hotmail.com>
2024-04-23 13:41:57 +0200

    Add docstring for task_wait_n function in python_async_function

--- python_async_function/4-tasks.py
+++ python_async_function/4-tasks.py
@@ -11,6 +11,7 @@ task_wait_random = __import__('3-tasks').task_wait_random
 
 
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    """calling task_wait_random"""
     tasks = [task_wait_random(max_delay) for i in range(n)]
     delays = await asyncio.gather(*tasks)
     return sorted(delays)
