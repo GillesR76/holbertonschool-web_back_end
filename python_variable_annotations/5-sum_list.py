b4d1607cbc9c5d499a5a881b3a258aafec7121f6
GillesR76 <gilles-richard76@hotmail.com>
2024-04-23 13:29:24 +0200

    Add type hint for sum_list and element_length functions in python_variable_annotations

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -5,7 +5,9 @@
 input_list of floats as argument and returns their sum as a float.
 """
 
+from typing import List
 
-def sum_list(input_list: list[float]) -> float:
+
+def sum_list(input_list: List[float]) -> float:
     """returns their sum as a float"""
     return sum(input_list)
--- python_variable_annotations/9-element_length.py
+++ python_variable_annotations/9-element_length.py
@@ -9,4 +9,5 @@ from typing import List, Tuple, Iterable, Sequence
 
 
 def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    """return values with the appropriate types"""
     return [(i, len(i)) for i in lst]
