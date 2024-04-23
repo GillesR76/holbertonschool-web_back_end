eb073e6c84f3a66e585f1875b279dd803d7e0635
GillesR76 <gilles-richard76@hotmail.com>
2024-04-23 13:35:05 +0200

    Add type hint for wait_random function in python_async_function

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -11,7 +11,7 @@ import asyncio
 import random
 
 
-async def wait_random(max_delay=10):
+async def wait_random(max_delay: int = 10) -> float:
     """asynchronous coroutine that takes in an integer argument"""
     random_delay = random.uniform(0, max_delay)
     await asyncio.sleep(random_delay)
