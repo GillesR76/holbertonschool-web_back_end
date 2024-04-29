697fe230372798020f849e9ca435e30e167af98b
GillesR76 <gilles-richard76@hotmail.com>
2024-04-29 18:16:31 +0200

    Update MongoDB connection in NoSQL/12-log_stats.py

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -8,7 +8,7 @@ from pymongo import MongoClient
 
 
 if __name__ == "__main__":
-    client = MongoClient('mongodb://127.0.0.1:27017')
+    client = MongoClient()
     logs_db = client.logs
     nginx_collection = logs_db.nginx
     x = nginx_collection.count_documents({})
