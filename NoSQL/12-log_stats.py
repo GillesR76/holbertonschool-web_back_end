f41d4e841c1cb1ce21449a068372cc4b8f03d6d9
GillesR76 <gilles-richard76@hotmail.com>
2024-04-30 09:03:08 +0200

    Refactor log_stats script to improve MongoDB connection and count_status variable assignment in NoSQL/12-log_stats.py

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -13,7 +13,7 @@ if __name__ == "__main__":
     logs_db = client.logs
     nginx_collection = logs_db.nginx
     x = nginx_collection.count_documents({})
-    print(f"{x}: logs")
+    print(f"{x} logs")
     print("Methods:")
     methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
     for method in methods:
