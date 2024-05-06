256d75b3ca3f66f9d007d3810d1787f5122e9b53
GillesR76 <gilles-richard76@hotmail.com>
2024-05-06 11:44:37 +0200

    Refactor block-scoped variables in ES6 basic file

--- ES6_basic/1-block-scoped.js
+++ ES6_basic/1-block-scoped.js
@@ -1,10 +1,12 @@
 export default function taskBlock(trueOrFalse) {
-  let task = false;
-  let task2 = true;
+  const task = false;
+  const task2 = true;
 
   if (trueOrFalse) {
-    task = true;
-    task2 = false;
+    // eslint-disable-next-line
+    const task = true;
+    // eslint-disable-next-line
+    const task2 = false;
   }
 
   return [task, task2];
