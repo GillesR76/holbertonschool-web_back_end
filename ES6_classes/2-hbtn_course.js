0ec314bf09fb144baa4d3f0ef21530a8b974b29c
GillesR76 <gilles-richard76@hotmail.com>
2024-05-06 18:37:33 +0200

    Add HolbertonCourse class in ES6_classes/2-hbtn_course.js

---
+++ ES6_classes/2-hbtn_course.js
@@ -0,0 +1,43 @@
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    this._name = name;
+    this._length = length;
+    this._students = students;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(newName) {
+    if (typeof newName === 'string') {
+      this._name = newName;
+    } else {
+      throw new TypeError('Name must be a string');
+    }
+  }
+
+  get length() {
+    return this._length;
+  }
+
+  set length(newlength) {
+    if (typeof newlength === 'number') {
+      this._length = newlength;
+    } else {
+      throw new TypeError('Length must be a number');
+    }
+  }
+
+  get students() {
+    return this._students;
+  }
+
+  set students(newStudents) {
+    if (Array.isArray(newStudents)) {
+      this._students = newStudents;
+    } else {
+      throw new TypeError('Students must be an array');
+    }
+  }
+}
