53de319b76b22176af70ebc270cb2b68c6fa2abf
GillesR76 <gilles-richard76@hotmail.com>
2024-05-06 11:19:29 +0200

    Add ES6 basic block-scoped function

---
+++ ES6_basic/1-block-scoped.js
@@ -0,0 +1,11 @@
+export default function taskBlock(trueOrFalse) {
+  let task = false;
+  let task2 = true;
+
+  if (trueOrFalse) {
+    task = true;
+    task2 = false;
+  }
+
+  return [task, task2];
+}
