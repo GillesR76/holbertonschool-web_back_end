267f869b52d4e7d295a7e65781b645eab1d5c35a
GillesR76 <gilles-richard76@hotmail.com>
2024-05-06 22:59:58 +0200

    Refactor ES6_classes/9-hoisting.js to export default listOfStudents

--- ES6_classes/9-hoisting.js
+++ ES6_classes/9-hoisting.js
@@ -42,4 +42,5 @@ const student3 = new StudentHolberton('Albert', 'Clinton', class2019);
 const student4 = new StudentHolberton('Donald', 'Bush', class2019);
 const student5 = new StudentHolberton('Jason', 'Sandler', class2019);
 
-export const listOfStudents = [student1, student2, student3, student4, student5];
+const listOfStudents = [student1, student2, student3, student4, student5];
+export default listOfStudents;
