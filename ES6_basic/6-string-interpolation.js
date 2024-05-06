78589cddbeaa205beb4c361c1d93969402f761fa
GillesR76 <gilles-richard76@hotmail.com>
2024-05-06 13:20:40 +0200

    Refactor string interpolation in ES6 basic file

--- ES6_basic/6-string-interpolation.js
+++ ES6_basic/6-string-interpolation.js
@@ -6,6 +6,6 @@ export default function getSanFranciscoDescription() {
     capita: '$178,479',
   };
 
-  return `As of ${year}, it was the seventh-highest income county in the United States, with a per capita personal income of ${budget.income}. ` +
-  `\nAs of 2015, San Francisco proper had a GDP of ${budget.gdp}, and a GDP per capita of ${budget.capita}.`;
+  return `As of ${year}, it was the seventh-highest income county in the United States, with a per capita personal income of ${budget.income}. `
+  + `\nAs of 2015, San Francisco proper had a GDP of ${budget.gdp}, and a GDP per capita of ${budget.capita}.`;
 }
