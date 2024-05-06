fb80dd4fab13fdb6d4aa890792b9a73ed2cfea4e
GillesR76 <gilles-richard76@hotmail.com>
2024-05-06 14:10:50 +0200

    Refactor getBudgetObject function to use object literal notation in 7-getBudgetObject.js

--- ES6_basic/7-getBudgetObject.js
+++ ES6_basic/7-getBudgetObject.js
@@ -1,3 +1,9 @@
 export default function getBudgetObject(income, gdp, capita) {
-  return { income, gdp, capita };
+  const budget = {
+    income,
+    gdp,
+    capita,
+  };
+
+  return budget;
 }
