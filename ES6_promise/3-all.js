a125c9971e15a971176732ff39c1cb96a7aee40f
GillesR76 <gilles-richard76@hotmail.com>
2024-05-13 16:37:56 +0200

    feat: Add handleProfileSignup function
    The commit adds a new function called handleProfileSignup to the ES6_promise/3-all.js file. This function uses the ES6 Promise.all method to upload a photo and create a user simultaneously. It then logs the photo body, user's first name, and last name. If there is an error, it logs that the signup system is offline.

---
+++ ES6_promise/3-all.js
@@ -0,0 +1,11 @@
+import { uploadPhoto, createUser } from './utils';
+
+export default function handleProfileSignup() {
+  return Promise.all([uploadPhoto(), createUser()])
+    .then(([photo, user]) => {
+      console.log(`${photo.body} ${user.firstName} ${user.lastName}`);
+    })
+    .catch(() => {
+      console.log('Signup system offline');
+    });
+}
