87882aed64d16f5336f77bfb2852811aaaf6a955
GillesR76 <gilles-richard76@hotmail.com>
2024-05-13 17:17:10 +0200

    feat: Update import statement for uploadPhoto in handleProfileSignup function

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,5 +1,5 @@
 import signUpUser from './4-user-promise';
-import { uploadPhoto } from './utils';
+import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
