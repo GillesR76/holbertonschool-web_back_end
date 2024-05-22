0e14d632495489f47e24d3fb6c931ba150f099e2
GillesR76 <gilles-richard76@hotmail.com>
2024-05-22 09:50:57 +0200

    function that returns an array of student objects

---
+++ ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,9 @@
+export default function getListStudents(id, firstName, location) {
+  const arrayList =
+    [
+      { id: 1, firstName: "Guillaume", location: "San Francisco" },
+      { id: 2, firstName: "James", location: "Columbia" },
+      { id: 3, firstName: "Serena", location: "San Francisco" }
+    ];
+  return arrayList;
+}
