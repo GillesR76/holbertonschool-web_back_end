86594dc61705234931aa55749434d2a3165ee3a2
GillesR76 <gilles-richard76@hotmail.com>
2024-05-22 11:36:11 +0200

    fix: Handle negative position in createInt8TypedArray function

--- ES6_data_manipulation/5-typed_arrays.js
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -1,7 +1,7 @@
 export default function createInt8TypedArray(length, position, value) {
   const buffer = new ArrayBuffer(length);
   const int8View = new Int8Array(buffer);
-  if (position >= length) {
+  if (position >= length || position < 0) {
     throw new Error('Position outside range');
   }
   int8View[position] = value;
