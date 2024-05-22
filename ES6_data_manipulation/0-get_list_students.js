53d0dc979bc4d45409ccd9e7da2280fe7738b31c
GillesR76 <gilles-richard76@hotmail.com>
2024-05-22 09:54:19 +0200

    adding a missing coma

--- ES6_data_manipulation/0-get_list_students.js
+++ ES6_data_manipulation/0-get_list_students.js
@@ -3,7 +3,7 @@ export default function getListStudents(id, firstName, location) {
     [
       { id: 1, firstName: "Guillaume", location: "San Francisco" },
       { id: 2, firstName: "James", location: "Columbia" },
-      { id: 3, firstName: "Serena", location: "San Francisco" }
+      { id: 3, firstName: "Serena", location: "San Francisco" },
     ];
   return arrayList;
 }
