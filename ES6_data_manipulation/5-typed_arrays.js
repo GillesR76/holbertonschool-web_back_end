8884e7ab0049f561011ada45fb27954c05f6a716
GillesR76 <gilles-richard76@hotmail.com>
2024-05-22 12:53:08 +0200

    fix: Use DataView instead of Int8Array in createInt8TypedArray function

--- ES6_data_manipulation/5-typed_arrays.js
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -1,9 +1,9 @@
 export default function createInt8TypedArray(length, position, value) {
   const buffer = new ArrayBuffer(length);
-  const int8View = new Int8Array(buffer);
+  const int8View = new DataView(buffer);
   if (position >= length || position < 0) {
     throw new Error('Position outside range');
   }
-  int8View[position] = value;
+  int8View.setInt8(position, value);
   return int8View;
 }
